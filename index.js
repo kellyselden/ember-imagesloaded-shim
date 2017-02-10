fbc6de16c172e8c989e1ca5d245d2496309a1e4c
Kelly Selden <kellyselden@gmail.com>
2017-02-09 16:49:33 -0800

    don't import if fastboot

--- index.js
+++ index.js
@@ -3,21 +3,33 @@
 var path = require('path');
 var mergeTrees = require('broccoli-merge-trees');
 
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
+
 module.exports = {
   name: 'ember-imagesloaded-shim',
   included: function(app) {
     this._super.included.apply(this, arguments);
-    app.import({
-      development: 'vendor/imagesloaded.pkgd.js',
-      production: 'vendor/imagesloaded.pkgd.min.js'
-    });
-    app.import('vendor/shims/imagesloaded.js');
+
+    if (!isFastBoot()) {
+      app.import({
+        development: 'vendor/imagesloaded.pkgd.js',
+        production: 'vendor/imagesloaded.pkgd.min.js'
+      });
+      app.import('vendor/shims/imagesloaded.js');
+    }
+
     return app;
   },
   treeForVendor: function(vendorTree) {
     var trees = [];
-    var imagesLoadedPath = path.dirname(require.resolve('imagesloaded'));
-    trees.push(imagesLoadedPath);
+
+    if (!isFastBoot()) {
+      var imagesLoadedPath = path.dirname(require.resolve('imagesloaded'));
+      trees.push(imagesLoadedPath);
+    }
+
     if (vendorTree) {
         trees.push(vendorTree);
     }
